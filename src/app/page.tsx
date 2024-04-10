99750345d21d81cbea316d4ff0a1e7d8ee18fcbb
erwinagpasa <rootaccess000@gmail.com>
2024-04-10 14:46:04 +0300

    adding logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from 'next-auth';
 import { options } from './api/auth/[...nextauth]/options';
 import { redirect } from 'next/navigation';
+import { signOut } from 'next-auth/react';
+import Navbar from './components/navbar/Navbar';
 
 
 
@@ -14,14 +16,22 @@ export default async function Home() {
 
 
   return (
-    <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50">
-      <div>
-        <div className="w-full md:w-6/12 lg:5/12 mx-auto flex h-screen justify-center items-center">
-          <div>
-            Active Session
+    <>
+      <Navbar user={session?.user} pagetype={"Server"} />
+      <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50">
+        <div>
+          <div className="w-full md:w-6/12 lg:5/12 mx-auto flex h-screen justify-center items-center">
+            <div>
+
+
+              <p>Active Session</p>
+
+
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
